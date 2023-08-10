164b27379c3381dc43c351bf61d72182387b1b65
SoumenBaral <shumen.sb@gmail.com>
2023-08-11 04:45:34 +0600

    Strat Assignment

---
+++ 2.Week-2/1.MagoreDistance.cpp
@@ -0,0 +1,42 @@
+#include<bits/stdc++.h>
+using namespace std;
+const int N = 1e5+5;
+vector<int>adj[N];
+bool visited[N];
+int level[N];
+void bfs(int s){
+    queue<int>q;
+    q.push(s);
+    visited[s]=true;
+    level[s]=0;
+    while(!q.empty()){
+         int u =q.front();
+            q.pop();
+            cout<<"Node level "<<level[u] <<" : "<<u<<endl;
+        for(int v: adj[u]){
+            if(visited[v]==true)continue;
+            q.push(v);
+            visited[v]=true;
+            level[v]=level[u]+1;
+        }
+    }
+    
+}
+int main ()
+{
+    int n,m;
+    cin>>n>>m;
+    for(int i=0;i<m;i++)
+    {
+        int u,v;
+        cin>>u>>v;
+        adj[u].push_back(v);
+        adj[v].push_back(u);
+    }
+    bfs(1);
+
+    for(int i=1;i<=n;i++){
+        cout<<"Level "<<i<< ": "<<level[i]<<endl;
+    }
+    return 0;
+}
