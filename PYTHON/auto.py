18a64d0d7b7987952fc9dab190a09371c29f2648
SoumenBaral <shumen.sb@gmail.com>
2023-11-08 02:46:25 +0600

    all is ok

--- PYTHON/auto.py
+++ PYTHON/auto.py
@@ -1,10 +1,10 @@
 import pyautogui
 
+
 n = int(input())
 if n == 1:
     print('#')
 else: 
-    # pyautogui.write('#',interval=0.25)
     for i in range(n+1):
         for j in range(0,i,1):
             pyautogui.write('#',interval=0.25)
